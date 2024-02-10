03e81f2b4dfba13007b968b5a6723b537a9eeda4
Sujan Singh Dangi <sujandangi@gmail.com>
2024-02-10 15:19:27 +0530

    Complete episode 3.

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-const heading = React.createElement("h1", {id: "heading"}, "Hello World from React CDN")
-
-console.log(heading)
+// const heading = React.createElement("h1", {id: "heading"}, "Hello World from React CDN")
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-console.log("root ", root)
+// React functional component
+const HeadingComponent = () => {
+    return (
+        <div>
+            <h2>I am react functional component.</h2>
+        </div>
+    )
+}
 
-root.render(heading)
+// React Element. We can nest a component inside an element and vice versa.
+const jsxHeading = <div id="heading">I am JSX/React element!
+    <HeadingComponent />
+</div>
+
+root.render(jsxHeading)
