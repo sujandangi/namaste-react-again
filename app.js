785ac17c155517aa2175c8e24ebbed5f4a1a8046
Sujan Singh Dangi <sujandangi@gmail.com>
2024-02-11 22:05:27 +0530

    Midway episode 4. Built basic app layout with data fetching from API.

--- app.js
+++ app.js
@@ -1,22 +1,109 @@
-import React from "react"
+import React, { useState } from "react"
 import ReactDOM from "react-dom/client"
+import { useState, useEffect } from "react"
 
-// const heading = React.createElement("h1", {id: "heading"}, "Hello World from React CDN")
-
-const root = ReactDOM.createRoot(document.getElementById("root"))
-
-// React functional component
-const HeadingComponent = () => {
+const Header = () => {
     return (
-        <div>
-            <h2>I am react functional component.</h2>
+        <div className="header">
+            <div className="logoContainer">
+                <img alt="Logo" src="" />
+                <p>Location</p>
+            </div>
+            <div className="navMenu">
+                <ul>
+                    <li>Offers</li>
+                    <li>Help</li>
+                    <li>Sign In</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
         </div>
     )
 }
 
-// React Element. We can nest a component inside an element and vice versa.
-const jsxHeading = <div id="heading">I am JSX/React element!
-    <HeadingComponent />
-</div>
+const SearchRestaurants = () => {
+    let [searchValue, setSearchValue] = useState("")
 
-root.render(jsxHeading)
+    return (
+        <div className="searchContainer">
+            <input type="text" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
+            <button>Search</button>
+        </div>
+    )
+}
+
+const RestaurantCard = ({ res }) => {
+    return (
+        <div className="restaurantCard">
+            <img alt="res-image" src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${res.info.cloudinaryImageId}`}/>
+            <div className="restaurantDescription">
+                <ul> 
+                    <li>{res.info.name}</li>
+                    <li>{res.info.avgRating}</li>
+                    <li>{res.info.sla.slaString}</li>
+                    <li>{res.info.costForTwo}</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const RestaurantCards = () => {
+    let [restaurants, setRestaurants] = useState([])
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.71700&lng=75.83370');
+                const json = await response.json();
+                // console.log(json);
+                // console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants[0].info.name);
+                setRestaurants(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        }
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        console.log("Restaurants state updated now (second useEffect)")
+        console.log("second useEffect", restaurants)
+    }, [restaurants])
+
+    return (
+        <div className="restaurantCardsContainer">
+            {restaurants.map(res => {
+                return <RestaurantCard res={res} key={res.info.id} />
+            })}
+        </div>
+    )
+}
+
+const RestaurantContainer = () => {
+    return (
+        <div>
+            <SearchRestaurants />
+            <RestaurantCards />
+        </div>
+    )
+}
+
+const Footer = () => {
+    return (
+        <div className="footerContainer">Footer</div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <>
+            <Header />
+            <RestaurantContainer />
+            <Footer />
+        </>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<AppLayout />)
